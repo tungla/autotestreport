5e2f2708d4357b901d4caa6fda5ee187a0bd9511
TungVuHoangMinh <TungVuHoangMinh>
2017-05-27 01:13:57 +0700

    - Update queries in Testresult model - Update excel export format

--- app/models/testresult.rb
+++ app/models/testresult.rb
@@ -1,6 +1,8 @@
 class Testresult < ApplicationRecord
 	validates_presence_of :test_case_name, :feature_name, :test_result
 
+	@time_zone = 'Asia/Ho_Chi_Minh'
+
 	def self.get_distinct_feature_name
 		# Testresult.find_by_sql ["select distinct feature_name from testresults where feature_name != ''"]
 		Testresult.select(:feature_name).distinct.where("feature_name != ''")
@@ -24,18 +26,20 @@ class Testresult < ApplicationRecord
 	end
 
 	def self.get_failed_test_results_at_exact_date date
-		Testresult.select("DISTINCT ON (test_case_name) test_case_name,*").where("test_result = 'Failed' and created_at::date = date '#{date}'").order('test_case_name, created_at desc')
+		date = date.in_time_zone(@time_zone)
+		Testresult.select("DISTINCT ON (test_case_name) test_case_name,*").where("test_result = 'Failed' and (created_at AT TIME ZONE 'UTC' AT TIME ZONE '#{@time_zone}')::date = date '#{date}'").order('test_case_name, created_at desc')
 	end
 
 	def self.get_passed_test_results_with_time_range range
-		Testresult.select("DISTINCT test_case_name").where("test_result = 'Passed' and created_at::date > current_date - #{range}")
+		Testresult.select("DISTINCT test_case_name").where("test_result = 'Passed' and (created_at AT TIME ZONE 'UTC' AT TIME ZONE '#{@time_zone}')::date > (current_date - #{range}) AT TIME ZONE 'UTC' AT TIME ZONE '#{@time_zone}' ")
 	end
 
 	def self.get_total_test_results_with_time_range range
-		Testresult.select("DISTINCT test_case_name").where("created_at::date > current_date - #{range}")
+		Testresult.select("DISTINCT test_case_name").where("(created_at AT TIME ZONE 'UTC' AT TIME ZONE '#{@time_zone}')::date > (current_date - #{range}) AT TIME ZONE 'UTC' AT TIME ZONE '#{@time_zone}'")
 	end
 
 	def self.get_test_result_with_exact_date date
-		Testresult.select("DISTINCT ON (test_case_name) test_case_name,*").where("created_at::date = date '#{date}'").order('test_case_name, created_at desc')
+		date = date.in_time_zone(@time_zone)
+		Testresult.select("DISTINCT ON (test_case_name) test_case_name,*").where("(created_at AT TIME ZONE 'UTC' AT TIME ZONE '#{@time_zone}')::date = date '#{date}'").order('test_case_name, created_at desc')
 	end
 end
